d68aa0860cca38b96dbe89d75d1d34c14b494b9e
Jon Botros <joneb@Jons-MacBook-Pro.local>
2021-12-31 17:32:02 -0500

    added backend route and custom fields for youth form

--- util/custom_fields.js
+++ util/custom_fields.js
@@ -1,5 +1,6 @@
 
 module.exports = {
+    // elevate youth fields
     school: "335300",
     invited_by: "335301",
     go_to_church:"335302",
@@ -7,13 +8,17 @@ module.exports = {
     hobbies: "335304",
     how_you_heard: "335305",
     parents_name: "335306",
-    parents_email:"335307",
-    instagram:"335308",
-    twitter:"335311",
-    snap:"335309",
-    tiktok:"335310",
-    facebook:"335312",
-    todays_date:"336018",
-    prayer_requests: "336019"
+    parents_email: "335307",
+    instagram: "335308",
+    twitter: "335311",
+    snap: "335309",
+    tiktok: "335310",
+    facebook: "335312",
+    todays_date: "336018",
+    prayer_requests: "336019",
+    // lcf church service fields
+    lcf_prayer_requests: "512479",
+    lcf_todays_date: "512481",
+    lcf_how_you_heard: "512482",
+    lcf_like_to_know_about: "512492",
 }
-
